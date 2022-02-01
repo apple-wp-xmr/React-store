0a70ca42ff66fce6510e2a19bbb8b916adb54ff9
Dmytro Martus <funbrainfun@gmail.com>
2022-02-01 16:06:13 +0200

    Auth0

--- src/index.js
+++ src/index.js
@@ -8,13 +8,25 @@ import { CartProvider } from './context/cart_context';
 import { UserProvider } from './context/user_context';
 import { Auth0Provider } from '@auth0/auth0-react';
 
+//dev-re7hlf4i.eu.auth0.com
+//fMKDd5Am82Sk2Xn0m8zxUSqNrf3TrqMs
+
 ReactDOM.render(
-  <ProductsProvider>
-    <FilterProvider>
-      <CartProvider>
-        <App />
-      </CartProvider>
-    </FilterProvider>
-  </ProductsProvider>,
+  <Auth0Provider
+    domain='dev-re7hlf4i.eu.auth0.com'
+    clientId='fMKDd5Am82Sk2Xn0m8zxUSqNrf3TrqMs'
+    redirectUri={window.location.origin}
+    CacheLocation='localstorage'
+  >
+    <UserProvider>
+      <ProductsProvider>
+        <FilterProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </FilterProvider>
+      </ProductsProvider>
+    </UserProvider>
+  </Auth0Provider>,
   document.getElementById('root')
 );
